9629c8fbee36ae708fca90071e6f34353c613608
Anuradha <anumo@dtu.dk>
2019-07-19 12:44:51 +0200

    first xcode project

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Anuradha Mohanty on 19/07/2019.
+//  Copyright © 2019 Anuradha Mohanty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
